321677c8341c33b90527d2dd9e4bb1b371071afe
Mandy Yan <mandy.yan.reg@gmail.com>
2017-07-06 11:18:57 +0800

    Initial commit

--- src/main/java/com/hitrate/config/DBConfig.java
+++ src/main/java/com/hitrate/config/DBConfig.java
@@ -28,10 +28,20 @@ public class DBConfig {
 	@Bean
 	public DataSource getDataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://ec2-23-21-96-70.compute-1.amazonaws.com:5432/da69r1m274m8g2");
-		dataSource.setUsername("wylxunytndnrxt");
-		dataSource.setPassword("c7939e79f9e4f455ca403f491b34ffea75aaa2ca2be676cbf3588e67c2356b5c");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//		dataSource.setUrl("jdbc:mysql://localhost:3306/hitratereport");
+//		dataSource.setUsername("root");
+//		dataSource.setPassword("");
+		dataSource.setUrl("jdbc:mysql://us-cdbr-iron-east-03.cleardb.net/heroku_5f297a3ec8aa0c9");
+		dataSource.setUsername("b1c67d63499810");
+		dataSource.setPassword("5ce77e92");
+//		dataSource.setDriverClassName("org.postgresql.Driver");
+//		dataSource.setUrl("jdbc:postgresql://localhost:5432/hitratereport");
+//		dataSource.setUsername("mandyyan");
+//		dataSource.setPassword("");
+//		dataSource.setUrl("jdbc:postgresql://ec2-23-21-96-70.compute-1.amazonaws.com:5432/da69r1m274m8g2");
+//		dataSource.setUsername("wylxunytndnrxt");
+//		dataSource.setPassword("c7939e79f9e4f455ca403f491b34ffea75aaa2ca2be676cbf3588e67c2356b5c");
 		return dataSource;
 	}
 
--- src/main/resources/db/migration/V1__create_table_hitrate.sql
+++ src/main/resources/db/migration/V1__create_table_hitrate.sql
@@ -1,8 +1,15 @@
-CREATE SEQUENCE hitrate_id_seq;
+--CREATE SEQUENCE hitrate_id_seq;
+--CREATE TABLE hitrate (
+--  id int NOT NULL DEFAULT nextval('hitrate_id_seq'),
+--  visit_date date NOT NULL,
+--  website varchar(1000) NOT NULL,
+--  visits int NOT NULL
+--);
+--ALTER SEQUENCE hitrate_id_seq OWNED BY hitrate.id;
 CREATE TABLE hitrate (
-  hitrate_id int NOT NULL DEFAULT nextval('hitrate_id_seq'),
-  visit_date date NOT NULL,
-  website varchar(1000) NOT NULL,
-  visits int NOT NULL
+     id int(5) NOT NULL AUTO_INCREMENT,
+	 visit_date date NOT NULL,
+	 website varchar(1000) NOT NULL,
+     visits int(5) NOT NULL,
+     PRIMARY KEY (id)
 );
-ALTER SEQUENCE hitrate_id_seq OWNED BY hitrate.hitrate_id;
