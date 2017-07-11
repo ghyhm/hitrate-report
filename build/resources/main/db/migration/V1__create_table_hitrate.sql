949a8a945ccb66a47f14264e3e2fce1047848c8a
Mandy Yan <mandy.yan.reg@gmail.com>
2017-07-11 13:23:50 +0800

    Initial commit

--- build/resources/main/db/migration/V1__create_table_hitrate.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE hitrate (
-     id int(5) NOT NULL AUTO_INCREMENT,
-	 visit_date date NOT NULL,
-	 website varchar(1000) NOT NULL,
-     visits int(5) NOT NULL,
-     PRIMARY KEY (id)
-);
